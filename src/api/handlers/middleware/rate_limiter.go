645d7828b2956a78ed32a5014e20455d2ab7255b
mkabdelrahman <mkcodedev@gmail.com>
2024-08-28 19:37:58 +0300

    add ip based rate limiting

--- src/api/handlers/middleware/rate_limiter.go
+++ src/api/handlers/middleware/rate_limiter.go
@@ -1,7 +1,11 @@
 package middleware
 
 import (
+	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	v1 "bluelight.mkcodedev.com/src/api/contracts/v1"
 	"bluelight.mkcodedev.com/src/api/handlers/errorhandler"
@@ -10,12 +14,50 @@ import (
 
 func RateLimiter(em *errorhandler.ErrorHandeler) middlewareFunc {
 	return func(next http.Handler) http.Handler {
-		limiter := rate.NewLimiter(2, 4)
+
+		type client struct {
+			limiter  *rate.Limiter
+			lastSeen time.Time
+		}
+
+		var (
+			mu      sync.Mutex
+			clients = make(map[string]*client)
+		)
+
+		go func() {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}()
+
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !limiter.Allow() {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				em.SendServerError(w, r, v1.ServerError{
+					InternalMessage: fmt.Sprintf("%s", err),
+					Code:            http.StatusInternalServerError,
+				})
+				return
+			}
+			mu.Lock()
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+			}
+
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
 				em.SendClientError(w, r, v1.TooManyRequestsError)
 				return
 			}
+			mu.Unlock()
 			next.ServeHTTP(w, r)
 		})
 	}
